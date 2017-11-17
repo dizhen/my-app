d488b8f639636fb66e79ef0771be317ce6e0cbdc
Zhen Di <zhendi@Zhens-MacBook.local>
2017-11-16 22:54:16 -0600

    started implementing the photos lightbox

--- src/Photographer/TopPhotographerPhotos.js
+++ src/Photographer/TopPhotographerPhotos.js
@@ -1,10 +1,141 @@
 import React, {Component} from 'react';
+import Lightbox from 'react-images';
+
+const images = [
+    {
+        src: "https://placeimg.com/480/480/animals",
+        orientation: 'square',
+        caption: 'Photo by Shawn',
+    },
+    {
+        src: "https://placeimg.com/480/320/animals",
+        orientation: 'landscape',
+        caption: 'Photo by Jane',
+    }
+];
+
+const gutter = {
+    small: 2,
+    large: 4,
+};
+
+const styles = {
+    gallery: {
+        marginRight: -gutter.small,
+        overflow: 'hidden',
+
+        '@media (minWidth: 500px)': {
+            marginRight: -gutter.large,
+        },
+    },
+
+    // anchor
+    thumbnail: {
+        boxSizing: 'border-box',
+        display: 'block',
+        float: 'left',
+        lineHeight: 0,
+        paddingRight: gutter.small,
+        paddingBottom: gutter.small,
+        overflow: 'hidden',
+
+        '@media (minWidth: 500px)': {
+            paddingRight: gutter.large,
+            paddingBottom: gutter.large,
+        },
+    },
+
+    // orientation
+    landscape: {
+        width: '30%',
+    },
+    square: {
+        paddingBottom: 0,
+        width: '40%',
+
+        '@media (minWidth: 500px)': {
+            paddingBottom: 0,
+        },
+    },
+    source:{
+        border: 0,
+        display: 'inline',
+        height: 'auto',
+        maxWidth: '50%',
+        width: 'auto',
+    }
+}
 
 export default class TopPhotographerPhotos extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            currentImage: 0,
+            lightboxIsOpen: false
+        };
+        this.openLightbox = this.openLightbox.bind(this);
+        this.closeLightbox = this.closeLightbox.bind(this);
+        this.gotoPrevious = this.gotoPrevious.bind(this);
+        this.gotoNext = this.gotoNext.bind(this);
+    }
+
+    openLightbox (index, event) {
+        event.preventDefault();
+        this.setState({
+            currentImage: index,
+            lightboxIsOpen: true,
+        });
+    }
+    closeLightbox () {
+        this.setState({
+            currentImage: 0,
+            lightboxIsOpen: false,
+        });
+    }
+    gotoPrevious () {
+        this.setState({
+            currentImage: this.state.currentImage - 1,
+        });
+    }
+    gotoNext () {
+        this.setState({
+            currentImage: this.state.currentImage + 1,
+        });
+    }
+
+    renderGallery() {
+        const gallery = images.map((obj, i) => {
+            //console.log(obj);
+            return (
+                <a
+                    href={obj.src}
+                    style={styles.thumbnail, styles[obj.orientation]}
+                    key={i}
+                    onClick={(e) => this.openLightbox(i, e)}
+                >
+                    <img src={obj.src} style={styles.source} />
+                </a>
+            );
+        });
+
+        return (
+            <div style={styles.gallery}>
+                {gallery}
+            </div>
+        );
+    }
+
     render() {
         return (
             <div>
-                photo 1, photo 2, etc.
+                {this.renderGallery()}
+                <Lightbox
+                    currentImage={this.state.currentImage}
+                    images={images}
+                    onClickNext={this.gotoNext}
+                    onClickPrev={this.gotoPrevious}
+                    isOpen={this.state.lightboxIsOpen}
+                    onClose={this.closeLightbox}/>
             </div>
         );
     }
