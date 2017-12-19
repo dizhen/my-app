4375faa06c7c0553554dda5a762bc72e62bbb26e
Xun Zhao <zxzx270@hotmail.com>
2017-12-18 20:50:22 -0600

    adjust footer layout

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -3,30 +3,31 @@ import React, {Component} from 'react';
 export default class Footer extends Component {
   render() {
     return (
-      <footer>
-        <div className="row mt-sm-5 text-center">
-          <div className="col-sm-3 col-xs-12 mt-lg-5">
+      <footer className="row mt-sm-5 pt-5 text-center">
+          <div className="offset-sm-2"/>
+          <div className="col-sm-2 col-xs-12">
             <a href="#">
               关于星影
             </a>
           </div>
-          <div className="col-sm-3 col-xs-12 mt-lg-5">
+          <div className="col-sm-2 col-xs-12">
             <a href="#">
               联系星影
             </a>
           </div>
-          <div className="col-sm-3 col-xs-12 mt-lg-5">
+          <div className="col-sm-2 col-xs-12">
             <a href="#">
               加入星影
             </a>
           </div>
-          <div className="col-sm-3 col-xs-12 mt-lg-5">
+          <div className="col-sm-2 col-xs-12">
             <a href="#">
               常见问题
             </a>
           </div>
-          <p className="mx-auto mt-md-4">Copyright © 2017 XYBL.com All Rights Reserved. 星影部落 版权所有</p>
-        </div>
+
+          <p className="mx-auto" style={{fontSize:'13px'}}>Copyright © 2017 XYBL.com All Rights Reserved. 星影部落 版权所有</p>
+
       </footer>
     );
   }
