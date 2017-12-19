b697a56a6e4b37e6d2d3e5f20e2d2c0b33705a5c
Zhen Di <zhendi@Zhens-MacBook.local>
2017-12-18 22:44:14 -0600

    added logo in navbar

--- src/Navigation/Navigation.js
+++ src/Navigation/Navigation.js
@@ -43,27 +43,9 @@ export default class Navigation extends Component {
   render() {
     return (
       <div className="row test">
-        {/*<nav className="navbar navbar-default">*/}
-
-        {/*<div className="navbar-header test">*/}
-        {/*<a className="navbar-brand" href="#">XingYing</a>*/}
-        {/*</div>*/}
-
-        {/*<div>*/}
-        {/*<ul className="nav navbar-nav test">*/}
-        {/*<li className="active"><a href="#">Home</a></li>*/}
-        {/*<li><a href="#">My profile</a></li>*/}
-        {/*<li><a href="#">My albums</a></li>*/}
-        {/*</ul>*/}
-        {/*</div>*/}
-
-        {/*<div className="nav navbar-right test">*/}
-        {/*{this.renderLogInSection()}*/}
-        {/*</div>*/}
-
-        {/*</nav>*/}
         <nav className="col navbar navbar-toggleable-md navbar-light bg-faded">
-          <a className="navbar-brand" href="#">Navbar</a>
+          <a className="navbar-brand" href="#"><img src="https://cdn.worldvectorlogo.com/logos/elastic-x-pack.svg"
+                                                    width={60} height={60}/></a>
           <button className="navbar-toggler" type="button" data-toggle="collapse"
                   data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
                   aria-expanded="false" aria-label="Toggle navigation">
